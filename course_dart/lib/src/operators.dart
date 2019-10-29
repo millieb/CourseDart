2e150fb22100d9ed7e731c6fa4a7e2377ecee7dd
millieb <mildred.brito@dataxport.net>
2019-10-29 07:38:13 -0600

    Operators

---
+++ course_dart/lib/src/operators.dart
@@ -0,0 +1,21 @@
+main(){
+  /*
+  We are able to work with expressions.
+  */
+
+  var a = 10;
+  var x = 20;
+
+  print(a <= x); //Prints the gratest value without an if exp
+  
+  /*Operators no necesitan estan adentro de una expression. 
+  Fuera de un if statement podemos usar comparadores
+  ==
+  !=
+  <
+  >
+  <=
+  >=
+  
+  */
+}
