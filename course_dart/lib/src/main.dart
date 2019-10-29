eb7dda91970d69ea451d6809446a07bd504074a4
millieb <mildred.brito@dataxport.net>
2019-10-29 07:03:59 -0600

    Numbers

--- course_dart/lib/src/main.dart
+++ course_dart/lib/src/main.dart
@@ -1,20 +1,3 @@
 main(){
-  /*Variables*/
-
-  //number int - double
-  //String: String nombre;
-  //Booleans
-  //Lists
-  //Maps
-  //Runes
-  //Symbols
-
-  var idioma; //All below are acceptable because we did not define the type
-  idioma = "ingles";
-  idioma = 0;
-  idioma = true;
-
-  String nombre; //Only String as type.
-  nombre = "Jose";
-  
+  print("Hola Mundo!");
 }
---
+++ course_dart/lib/src/numeros.dart
@@ -0,0 +1,11 @@
+numeros(){
+  var numero = 1; //variable numero es un entero 1
+
+  num x = 1; //Same as above. Num (es un tipo numero que incluye todos los numeros) esta funcionando como una palabra
+  x = 10.55; //puede tomar el valor de un numero entero o un double
+
+  int n = 1; //int se mantendra con un valor entero nada mas. 
+
+  double d = 10.55; //double
+  d = 1; //valido. requiere mucho espacio pero si es permitido.
+}
