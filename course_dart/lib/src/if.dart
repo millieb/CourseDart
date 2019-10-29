ffbc4abc63e6a77d7b9a7ed5b05c7105872ed8d0
millieb <mildred.brito@dataxport.net>
2019-10-29 07:50:48 -0600

    If

--- course_dart/lib/src/if.dart
+++ course_dart/lib/src/if.dart
@@ -1,3 +1,25 @@
 main(){
-  
+
+  var numero = 1;
+  var n = 10;
+
+/**Operadores que podemos utilizar en un if*/
+
+
+/**== != < > <= >= */
+  if (numero<n){
+    print("Numero es menor a n.");
+  } else{
+    print("Numero es mayor a n.");
+  }
+
+/**as, is, !is, */
+  print(numero is String);
+  print(numero is num);
+
+/**||, && */
+  if((numero < n || numero is num)){
+    print("numero es menor a n");
+  }
+
 }
