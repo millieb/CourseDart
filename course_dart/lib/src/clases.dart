41c019e1766c091bcf6705f71ac64ac8735ffaf9
millieb <mildred.brito@dataxport.net>
2019-10-30 06:34:33 -0600

    clases

---
+++ course_dart/lib/src/clases.dart
@@ -0,0 +1,13 @@
+
+class Persona{
+  String nombre;
+  int edad;
+}
+
+main(List<String> args) {
+  var mildred = new Persona();
+  mildred.nombre = "Mildred Brito";
+  mildred.edad = 26;
+
+  print(mildred.nombre);
+}
